3841047a2d0554b8cf36d9ab3fa51de35948b934
crobalee <crobalee@korea.kr>
2025-08-07 12:08:14 +0900

    Add files via upload

--- gradlew.bat
+++ gradlew.bat
@@ -79,7 +79,7 @@ if "%ERRORLEVEL%"=="0" goto mainEnd
 
 :fail
 rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
-rem the _cmd_ return code when the batch file is called from a command line.
+rem the _cmd_ return code when the batch file is called from the command line.
 if not "" == "%GRADLE_EXIT_CONSOLE%" exit 1
 exit /b 1
 
